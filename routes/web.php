c3891960fa2b42814d778d39e75c55ae72357f0d
omar tarek <omar.tarek@iVAS.local>
2021-01-24 11:12:06 +0200

    error user

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Auth::routes();
-
-//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/home', 'HomeController@index')->name('home');
-
-
+get_static_routes() ;
+get_dynamic_routes();
---
+++ storage/framework/views/5bf0b53a7dd48bb62c52f6480f77bda1383b7105.php
@@ -0,0 +1,22 @@
+
+<?php $__env->startSection('content'); ?>
+<!-- BEGIN Tiles -->
+    <?php if(Auth::user()->hasRole('super_admin')): ?>
+        <div class="row">
+            <div class="col-md-7">
+                <div class="row">
+                    <div class="col-md-6">
+                        <a class="tile tile-pink" data-stop="500" href="<?php echo e(url('users')); ?>">
+                            <div class="img img-center">
+                                <i class="fa fa-user"></i>
+                                <p class="title text-center">+<?php echo e($users); ?></p>
+                                <p class="title text-center"><?php echo app('translator')->get('messages.users.users'); ?></p>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    <?php endif; ?>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('template', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\ivas_template_laravel8.0\resources\views/dashboard/index.blade.php ENDPATH**/ ?>
