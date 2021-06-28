b8c279ed2e3a620d59f75b2f3e93791a20a7138b
omar tarek <omar.tarek@iVAS.local>
2021-06-28 12:44:29 +0200

    rename rbtcode

--- app/Models/Rbtcode.php
+++ app/Models/Rbtcode.php
@@ -9,17 +9,15 @@ class RbtCode extends Model
 {
     use Filterable;
 
-    protected $fillable = ['content_id','operator_id','rbt_code'];
+    protected $fillable = ['content_id', 'operator_id', 'rbt_code'];
 
     public function operator()
     {
-      return $this->belongsTo('App\Models\Operator','operator_id','id');
+        return $this->belongsTo('App\Models\Operator', 'operator_id', 'id');
     }
 
     public function content()
     {
-      return $this->belongsTo('App\Models\Content','content_id','id');
+        return $this->belongsTo('App\Models\Content', 'content_id', 'id');
     }
-
-
 }
